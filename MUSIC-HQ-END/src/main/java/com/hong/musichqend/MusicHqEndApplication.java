b8e548e3dbf58a90617daed248a1044a5eeb4472
HQ <19873968643@163.com>
2024-07-02 16:39:56 +0800

    初始工程

---
+++ MUSIC-HQ-END/src/main/java/com/hong/musichqend/MusicHqEndApplication.java
@@ -0,0 +1,13 @@
+package com.hong.musichqend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicHqEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicHqEndApplication.class, args);
+    }
+
+}
---
+++ MUSIC-HQ-END/src/test/java/com/hong/musichqend/MusicHqEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.musichqend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicHqEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
